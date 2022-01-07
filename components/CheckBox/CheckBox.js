05a184a0bd3799b03d8bc04432aced98d96382fb
Netanel Elikamel <netanelikamel@gmail.com>
2022-01-07 12:38:57 +0200

    change

---
+++ components/CheckBox/CheckBox.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import * as S from "./style";
+
+const CheckBox = ({ isChecked, onChange, label, value }) => {
+  const handleChange = () => {
+    onChange && onChange(value);
+  };
+  return (
+    <S.CheckBox>
+      <FormControlLabel
+        control={<Checkbox checked={isChecked} onChange={handleChange} color="primary" />}
+        label={label}
+      />
+    </S.CheckBox>
+  );
+};
+
+var number=5;
+
+export default CheckBox;
